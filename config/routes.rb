2ed692b344f19eb40215689f25ec09f4e1181ef1
cripps <rob.cripps@gmail.com>
2015-04-15 15:52:46 -0400

    Adding feature to delete pictures

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   patch 'pictures/:id' => "pictures#update"
 
   get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
